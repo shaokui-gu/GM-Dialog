4d0973aef1024c4486b88ce858576a70a303277c
gsk <gsk@kroknow.com>
2021-12-24 16:16:50 +0800

    update gm to 0.0.9

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["GM+Dialog"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/shaokui-gu/GM.git", from: "0.0.8"),
+        .package(url: "https://github.com/shaokui-gu/GM.git", from: "0.0.9"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
